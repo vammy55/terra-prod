9a5a4d3becbef2709ba8f23f58b8eb49bce79e91
vamsi <vamsee54@gmail.com>
2020-02-21 18:01:16 +0530

    sec1

--- Modules/Ec2/ec2.tf
+++ Modules/Ec2/ec2.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "ec2-server" {
       "sudo pip install ansible",
       "sudo yum install java-1.8.0 -y",
       "sudo yum remove java-1.7.0-openjdk -y",
-      "sudo yum install wget -y"
+      "sudo yum install wget -y",
       "sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo",
       "sudo rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key",
       "sudo yum install jenkins -y",
